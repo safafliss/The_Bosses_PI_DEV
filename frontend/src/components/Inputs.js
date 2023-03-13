d49a2e35a7deea8377462224c88cb1ef2f737600
Ons Ben Amor <60884713+ons33@users.noreply.github.com>
2023-03-13 20:55:41 +0100

    signinemail
    sign up email

--- frontend/src/components/Inputs.js
+++ frontend/src/components/Inputs.js
@@ -3,20 +3,17 @@ import Classnames from 'classnames'
 
 function Inputs({name, label,  type, icon, onChangeHandler,errors}) {
   return (
-    <div class=" mb-3">
-      <label class="form-label">{label}</label>
-      <div class="input-group">
-        <span class="input-group-text">
-          <i class={icon}></i>
-        </span>
-        <input type={type}  name={name} class={Classnames("form-control", {"is-invalid": errors})} onChange={onChangeHandler}/>
+    <>
+      <div >
+       
+        <input type={type}  name={name} class={Classnames("form-control", {"is-invalid": errors})} onChange={onChangeHandler} placeholder={name}/>
         {
           errors && (<div  className="invalid-feedback">
           {errors}
         </div>)
         }
       </div>
-    </div>
+    </>
   );
 }
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,69 +1,178 @@
-import React, { useState } from "react";
+import React  ,{ useState } from "react";
+import backgroundImage from '../assets/img/register_bg_2.png';
+import Inputs from "../components/Inputs";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import Inputs from "../components/Inputs";
- import { LoginAction } from "../redux/actions/authActions";
-
-function Login() {
-  const [form, setForm] = useState({})
-  const dispatch = useDispatch()
-  const errors = useSelector(state=>state.errors)
-  const navigate = useNavigate()
-  const onChangeHandler = (e)=>{
+import { LoginAction } from "../redux/actions/authActions";
+export default function Login() {
+  const [form, setForm] = useState({});
+  const dispatch = useDispatch();
+  const errors = useSelector((state) => state.errors);
+  const navigate = useNavigate();
+  const onChangeHandler = (e) => {
     setForm({
       ...form,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-
-  const onSubmit = (e)=>{
+  const onSubmit = (e) => {
     e.preventDefault();
-     dispatch(LoginAction(form, navigate))
-    }
-
+    dispatch(LoginAction(form, navigate));
+  };
   return (
-    <div class="container p-4 mt-4">
-    <div class="row justify-content-evenly mt-4">
-      <div class="col-lg-6 col-md-12 mt-4">
-        <div class="d-flex">
-          <i class="fa-solid fa-right-to-bracket fs-1 mx-2"></i>{" "}
-          <h2>Login</h2>
-        </div>
-        <div
-          class="p-6 shadow-lg p-3 mb-5 bg-body rounded"
-          style={{ backgroundColor: "white" }}
-        >
-          <form onSubmit={onSubmit}>
-            <Inputs
-              name="email"
-              label="Email"
-              type="text"
-              icon="fa-solid fa-at"
-              onChangeHandler={onChangeHandler}
-              errors={errors.email}
-            />
-            <Inputs
-              name="password"
-              label="Password"
-              type="password"
-              icon="fa-solid fa-key"
-              onChangeHandler={onChangeHandler}
-              errors={errors.password}
-          
-            />
-            <div class="d-flex justify-content-between">
-              <button type="submit" class="btn btn-outline-primary">
-                Save <i class="fa-solid fa-floppy-disk"></i>
-              </button>
-              <Link to="/register">I don't have account</Link>
+    <>
+         <main >
+        <section className="relative w-full h-full py-40 min-h-screen bg-blueGray-800 bg-no-repeat bg-ful"
+         style={{
+          backgroundImage: `url(${backgroundImage})`,
+  
+          backgroundSize: 'cover',
+        
+              }}>
+          <div
+            className="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+            style={{
+				backgroundImage: `url(${backgroundImage})`,
+
+				backgroundSize: 'cover',
+			
+            }}
+          ></div>
+      <div className="container mx-auto px-4 h-full" style={{
+              backgroundColor:"transparent",
+			     marginTop:'5%'
+
+			
+            }}>
+        <div className="flex content-center items-center justify-center h-full">
+          <div className="w-full lg:w-4/12 px-4"style={{
+           
+			     marginTop:'-5%'
+
+			
+            }} >
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+              <div className="rounded-t mb-0 px-6 py-6">
+                <div className="text-center mb-3">
+                  <h6 className="text-blueGray-500 text-sm font-bold">
+                    Sign in with
+                  </h6>
+                </div>
+                <div className="btn-wrapper text-center">
+                  <button
+                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    <img
+                      alt="..."
+                      className="w-5 mr-1"
+                      src={require("../assets/img/github.svg").default}
+                    />
+                    Github
+                  </button>
+                  <button
+                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    <img
+                      alt="..."
+                      className="w-5 mr-1"
+                      src={require("../assets/img/google.svg").default}
+                    />
+                    Google
+                  </button>
+                </div>
+                <hr className="mt-6 border-b-1 border-blueGray-300" />
+              </div>
+              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                <div className="text-blueGray-400 text-center mb-3 font-bold">
+                  <small>Or sign in with credentials</small>
+                </div>
+                <form onSubmit={onSubmit}>
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Email
+                    </label>
+                    <Inputs
+                     name="email"
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Email"
+                      onChangeHandler={onChangeHandler}
+                      errors={errors.email}
+                    />
+                  </div>
+
+                  <div className="relative w-full mb-3">
+                    <label
+                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      htmlFor="grid-password"
+                    >
+                      Password
+                    </label>
+                    <Inputs
+                       name="password"
+                       type="password"
+
+                       label="Password"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="Password"
+                      onChangeHandler={onChangeHandler}
+                      errors={errors.password}
+                    />
+                  </div>
+                  <div>
+                    <label className="inline-flex items-center cursor-pointer">
+                      <input
+                        id="customCheckLogin"
+                        type="checkbox"
+                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                      />
+                      <span className="ml-2 text-sm font-semibold text-blueGray-600">
+                        Remember me
+                      </span>
+                    </label>
+                  </div>
+
+                  <div className="text-center mt-6">
+                    <button
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="submit"
+                      name="signin"
+                    >
+                      Sign In
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-          </form>
+            <div className="flex flex-wrap mt-6 relative">
+              <div className="w-1/2">
+                <a
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                  className="text-blueGray-200"
+                >
+                  <small>Forgot password?</small>
+                </a>
+              </div>
+              <div className="w-1/2 text-right">
+                <Link to="/register" className="text-blueGray-200">
+                  <small>Create new account</small>
+                </Link>
+              
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  )
+     
+	  </section>
+      </main>
+    </>
+  );
 }
-
-export default Login
