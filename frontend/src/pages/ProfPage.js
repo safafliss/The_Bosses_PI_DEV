2a1301d8591b3c04d6faf084c09573c3a45191e8
safafliss <safa.fliss@esprit.tn>
2023-05-08 19:31:15 +0100

    ,

--- frontend/src/pages/ProfPage.js
+++ frontend/src/pages/ProfPage.js
@@ -7,6 +7,7 @@ function ProfPage() {
     <div className='wholePage'  style={{"background":"white"}}>
         <Navbar></Navbar>
     <img className='firstImage' style={{"width":"100%"}} src={foodImage} ></img>
+    <br/>
     <div className='partiProducts d-flex justify-content-center'  style={{"background":"white"}}>
         <div className='recherche d-flex justify-content-center w-100'  style={{background: "white","alignItems":"center"}}>
             <select style={{color:"black","borderRadius":"10px","border":"none"}}  >
@@ -24,11 +25,10 @@ function ProfPage() {
     </div>
     <div className='productsBel7a9 mt-5 ' style={{"background":"white"}}>
         <div className='superMarche bg-light'>
-            <h2 style={{"background":"white"}}>Super Marche</h2>
-            <div className='cardsMarche d-flex  flex-wrap'>
+            <div className='cardsMarche d-flex  flex-wrap center'>
                 
                 <div class="card cardMahdi"  >
-                            <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                            <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                             <div className='d-flex'>
                                 <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                                 <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -42,7 +42,7 @@ function ProfPage() {
 
                 
                             <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                         <div className='d-flex'>
                             <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                             <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -56,7 +56,38 @@ function ProfPage() {
 
                 
                         <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
+                        <div className='d-flex'>
+                            <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
+                            <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
+                        </div>
+                        <div class="card-body" style={{"paddingBottom": "1px"}}>
+                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
+                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
+                            <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
+                        </div>
+                        </div>
+            </div>
+
+                
+            <div className='cardsMarche d-flex  flex-wrap'>
+                
+                <div class="card cardMahdi"  >
+                            <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
+                            <div className='d-flex'>
+                                <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
+                                <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
+                            </div>
+                            <div class="card-body" style={{"paddingBottom": "1px"}}>
+                                <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
+                                <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
+                                <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
+                            </div>
+                            </div>
+
+                
+                            <div class="card cardMahdi"  >
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                         <div className='d-flex'>
                             <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                             <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -70,7 +101,7 @@ function ProfPage() {
 
                 
                         <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                         <div className='d-flex'>
                             <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                             <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -83,12 +114,11 @@ function ProfPage() {
                         </div>
             </div>
                     
-            <h2 style={{"background":"white"}}>Fruits</h2>
-                
+                    
             <div className='cardsMarche d-flex  flex-wrap'>
                 
                 <div class="card cardMahdi"  >
-                            <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                            <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                             <div className='d-flex'>
                                 <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                                 <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -102,7 +132,7 @@ function ProfPage() {
 
                 
                             <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                         <div className='d-flex'>
                             <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                             <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
@@ -116,81 +146,7 @@ function ProfPage() {
 
                 
                         <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
-                        <div className='d-flex'>
-                            <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
-                            <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
-                        </div>
-                        <div class="card-body" style={{"paddingBottom": "1px"}}>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
-                            <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
-                        </div>
-                        </div>
-
-                
-                        <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
-                        <div className='d-flex'>
-                            <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
-                            <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
-                        </div>
-                        <div class="card-body" style={{"paddingBottom": "1px"}}>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
-                            <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
-                        </div>
-                        </div>
-            </div>
-                    
-            <h2 style={{"background":"white"}}>Patesserie</h2>
-                    
-            <div className='cardsMarche d-flex  flex-wrap'>
-                
-                <div class="card cardMahdi"  >
-                            <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
-                            <div className='d-flex'>
-                                <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
-                                <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
-                            </div>
-                            <div class="card-body" style={{"paddingBottom": "1px"}}>
-                                <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
-                                <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
-                                <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
-                            </div>
-                            </div>
-
-                
-                            <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
-                        <div className='d-flex'>
-                            <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
-                            <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
-                        </div>
-                        <div class="card-body" style={{"paddingBottom": "1px"}}>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
-                            <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
-                        </div>
-                        </div>
-
-                
-                        <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
-                        <div className='d-flex'>
-                            <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
-                            <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
-                        </div>
-                        <div class="card-body" style={{"paddingBottom": "1px"}}>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-clock" aria-hidden="true"></i> Today 19:00 - 23:59</p>
-                            <p class="" style={{color:"#a5a5a5","textAlign": "initial","marginBottom": "1px"}}><i class="fa fa-map-marker" aria-hidden="true"></i> 2.0KM</p>
-                            <p class="" style={{color:"green","textAlign": "end","marginBottom": "1px"}}>3Dt</p>
-                        </div>
-                        </div>
-
-                
-                        <div class="card cardMahdi"  >
-                        <img class="card-img-top" src="https://i2.wp.com/www.addictgroup.fr/wp-content/uploads/2017/09/shopping-2613984_1920.jpg?fit=862%2C496&ssl=1" alt="Card image cap" />
+                        <img class="card-img-top" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freestufffinder.com%2Ffree-walmart-grocery-delivery-to-your-home-minimum-50-order-limited-time%2F&psig=AOvVaw0Ferua1THbQroKOwbr9n5x&ust=1683656746065000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCJj3xqus5v4CFQAAAAAdAAAAABAL" />
                         <div className='d-flex'>
                             <img style={{position:"absolute", marginTop:"-40px" ,marginLeft:"10px" ,height:"50px",width:"50px","borderRadius":"50%"}} src="https://www.radiofrance.fr/s3/cruiser-production/2023/01/c7bfb0de-590d-488d-bd48-7e548f6ff6ee/1200x680_gettyimages-482142025.jpg" ></img>
                             <p style={{position:"absolute", marginTop:"-35px","marginLeft":"70px","color":"white","fontSize":25}} ><b>Test ya bro</b></p>
