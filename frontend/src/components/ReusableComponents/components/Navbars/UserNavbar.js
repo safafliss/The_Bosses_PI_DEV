c400c6bb3b206485e9a95033a20f8ebcd7916cea
safafliss <safa.fliss@esprit.tn>
2023-05-10 00:40:27 +0100

    nav responsive

--- frontend/src/components/ReusableComponents/components/Navbars/UserNavbar.js
+++ frontend/src/components/ReusableComponents/components/Navbars/UserNavbar.js
@@ -105,7 +105,7 @@ export default function Navbar() {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button  style = {{"width": "50px","height": "50px"}} className="buttonNavMahdi inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                 </Disclosure.Button>
               </div>
